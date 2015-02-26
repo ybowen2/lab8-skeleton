3bb874a12ffd981c18bf0e41322b9b87fa9515c8
vagrant <vagrant@precise32.(none)>
2015-02-26 21:52:56 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,10 +12,14 @@ function initializePage() {
 	// add any functionality and listeners you want here
 
   $(".version_a").click(function(){
-    //add your Woopra tracking code for version A's like button click event
+
+  	woopra.track("a_version_like_click");
+  	    //add your Woopra tracking code for version A's like button click event
   })
 
   $(".version_b").click(function(){
+  	woopra.track("b_version_like_click");
+  	
     //add your Woopra tracking code for version A's like button click event
   })
 
